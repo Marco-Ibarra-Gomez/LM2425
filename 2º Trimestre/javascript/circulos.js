1b2ce21102b78ad475b0c98e17cb6df5e8069faf
Marco Ibarra Gómez <marcoantonioibarra03@iesamachado.org>
2024-12-17 10:47:58 +0100

    (17/12/2024)

---
+++ 2º Trimestre/javascript/circulos.js
@@ -0,0 +1,19 @@
+function rojo_h(){
+    document.getElementById('rojo').style.visibility='hidden';
+}
+function verde_h(){
+    document.getElementById('verde').style.visibility='hidden';
+}
+function azul_h(){
+    document.getElementById('azul').style.visibility='hidden';
+}
+
+function rojo_v(){
+    document.getElementById('rojo').style.visibility='visible';
+}
+function verde_v(){
+    document.getElementById('verde').style.visibility='visible';
+}
+function azul_v(){
+    document.getElementById('azul').style.visibility='visible';
+}
