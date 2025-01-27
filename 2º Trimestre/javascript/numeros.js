4b3f1fe6cf33e6d0eba975727fea67a6986ebfcd
Marco-Antonio-Ibarra <marcoantonioibarra03@iesamachado.org>
2025-01-27 19:43:29 +0100

    (27/01/2025)

--- 2º Trimestre/javascript/numeros.js
+++ 2º Trimestre/javascript/numeros.js
@@ -2,15 +2,15 @@ let array = [];
     function anadir(event){
         event.preventDefault();
         let valorArray0 = document.getElementById("numero").value;
-        let valorArray = valorArray0.replace(',', '.');
+        let valorArray;
         let valorArray1;
-        if(isNaN(valorArray) || valorArray === ''){
+        if(isNaN(valorArray0) || valorArray0 === ''){
             alert('Por favor, ingresa un número');
         }else{
+            valorArray = valorArray0.replace(',', '.');
             valorArray1 = parseFloat(valorArray);
+            array.push(valorArray1);
         }
-        array.push(valorArray1);
-        console.log(array);
         }
     function mostrar(event){
         event.preventDefault();
@@ -20,8 +20,7 @@ let array = [];
             document.getElementById("mostrar").innerHTML = array;
         }
     }
-    function mediaAritmetica(event){
-        event.preventDefault();
+    function suma(){
         let sumatorio = 0;
         if(array.length === 0){
             document.getElementById("media").innerHTML = 'No has introducido ningún valor en "Añadir"';
@@ -32,15 +31,54 @@ let array = [];
             document.getElementById("media").innerHTML = sumatorio;
         }
     }
-    function grande(event){
-        event.preventDefault();
-        
+    function grande(){
+        if(array.length === 0){
+            document.getElementById("mostrar").innerHTML = 'No has introducido ningún valor en "Añadir"';
+        }else{
+            let numeroGrande = array[0];
+            for(let i = 0; array.length>i; i++){
+                if(array[i]>numeroGrande){
+                    numeroGrande = array[i];
+                }
+            }
+            document.getElementById("media").innerHTML = numeroGrande;
+        }
     }
-    function pequeno(event){
-        event.preventDefault();
-
+    function pequeno(){
+        if(array.length === 0){
+            document.getElementById("mostrar").innerHTML = 'No has introducido ningún valor en "Añadir"';
+        }else{
+            let numeroMenor = array[0];
+            for(let i = 0; array.length>i; i++){
+                if(array[i]<numeroMenor){
+                    numeroMenor = array[i];
+                }
+            }
+            document.getElementById("media").innerHTML = numeroMenor;
+        }
     }
-    function posicion(event){
-        event.preventDefault();
-
+    function posicion_array(){
+        if(array.length === 0){
+            document.getElementById("pos").innerHTML = 'No has introducido ningún valor en "Añadir"';
+        }else{
+            let numeroP = document.getElementById("posicion").value;
+            let numeroP1;
+            let numeroP2;
+            if(isNaN(numeroP) || numeroP === ''){
+                alert('Por favor, ingresa un número');
+            }else{
+                numeroP1 = numeroP.replace(',', '.');
+                numeroP2 = parseFloat(numeroP1);
+                let encontrado = 0;       
+                for(let i = 0; array.length>i; i++){
+                    if(array[i] === numeroP2){
+                        document.getElementById("pos").innerHTML = "El número introducido está en la posición " + i;
+                        encontrado = 1;
+                    }
+                }
+                if(encontrado === 0){
+                    document.getElementById("pos").innerHTML = "El número introducido no coincide con ningún valor agregado en 'Añadir'";
+                }
+            }
+        }
     }
